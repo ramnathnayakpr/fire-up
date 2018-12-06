46580366cb3d4afe9176a77d7146915cd7dce9ee
Ramnath Nayak <ramnathnayakpr@gmail.com>
2018-12-06 21:14:45 +0000

    Fixed debug
    Fixed debug

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo '*** Running Docker Install ***'
+echo "*** Running Docker Install as $USER ***"
 curl -fsSL https://get.docker.com -o get-docker.sh
 
-echo '*** Running usermod ***'
+echo "*** Running usermod ***"
 echo "sudo usermod -aG docker ubuntu" >> get-docker.sh
 
-echo '*** usermod returned $? ***"
+echo "*** usermod returned $? ***"
 sh get-docker.sh
