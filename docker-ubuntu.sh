58ba6d4c09a64f6fd542df7d73685f562ab4fb3e
Ramnath Nayak <ramnathnayakpr@gmail.com>
2018-12-06 21:56:42 +0000

    Tweaked the order
    Tweaked the order

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -2,9 +2,7 @@
 
 echo "*** Running Docker Install as $USER ***"
 curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 
 echo "*** Running usermod ***"
 echo "sudo usermod -aG docker ubuntu" >> get-docker.sh
-
-echo "*** usermod returned $? ***"
-sh get-docker.sh
