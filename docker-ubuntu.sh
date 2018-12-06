13b4488e079bcbcb51641eaca5c18e8f4199f015
Ramnath Nayak <ramnathnayakpr@gmail.com>
2018-12-06 22:00:36 +0000

    Fixed issue
    Fixed issue

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -5,4 +5,4 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
 echo "*** Running usermod ***"
-echo "sudo usermod -aG docker ubuntu" >> get-docker.sh
+sudo usermod -aG docker ubuntu
